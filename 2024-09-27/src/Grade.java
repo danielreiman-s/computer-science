6c8542a9140433b8d7e028a847ed65077a59b889
Daniel Reiman <100288199+danielreiman@users.noreply.github.com>
2024-09-29 10:38:43 +0300

    Update Grade.java

--- 2024-09-27/src/Grade.java
+++ 2024-09-27/src/Grade.java
@@ -1,4 +1,32 @@
 
 public class Grade {
+	private String apparatus;
+	private double score;
+	
+	public Grade(String apparatus, double score) {
+		this.apparatus = apparatus;
+		this.score = score;
+	}
+	
+	public Grade(Grade orignal) {
+		this.apparatus = orignal.getApparatus();
+		this.score = orignal.getScore();
+	}
 
+	
+	public String getApparatus() {
+		return apparatus;
+	}
+
+	public void setApparatus(String apparatus) {
+		this.apparatus = apparatus;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
 }
