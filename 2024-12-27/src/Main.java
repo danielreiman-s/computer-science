dfd73df898a4a3b7e21515b03d0d1a2204688714
Daniel Reiman <100288199+danielreiman@users.noreply.github.com>
2024-12-27 13:00:30 +0200

    Update Main.java

--- 2024-12-27/src/Main.java
+++ 2024-12-27/src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		// Exercise 2
 		Node<Person> p1 = new Node<Person>(new Person("Someone", "One"));
 		Node<Person> p2 = new Node<Person>(new Person("Someone", "Two"));
 		Node<Person> p3 = new Node<Person>(new Person("Someone", "Three"));
@@ -9,20 +10,25 @@ public class Main {
 		p1.setNext(p2);
 		p2.setNext(p3);
 		
-		System.out.println(p1.toString());
-		System.out.println(insertGuests(p1));
+		// Exercise 3 + 4
+		System.out.println("Before insertGuests: " + p1.toString());
+		System.out.println("After insertGuests: " + insertGuests(p1));
 	}
 	public static Node<Person> insertGuests(Node<Person> head) {
 		Node<Person> newHead = head;
-		Node<Person> dummyNewHead = new Node<Person>(null);
+		Node<Person> dummyNewHead = new Node<Person>(null); // Exercise 5 using a dummy Node
+		Node<Person> current = dummyNewHead;
 		int guestCount = 1;
 		
-		while (newHead.hasNext()) {
-			Node<Person> guest = new Node<Person>(new Person("guest" , String.valueOf(guestCount)));
-			dummyNewHead.setNext(guest);
+		while (newHead != null) {
+			Node<Person> guest = new Node<Person>(new Person("guest" , "g" + String.valueOf(guestCount)));
+			current.setNext(guest);
+		
+			current = current.getNext();
+			
 			Node<Person> guestInfo = new Node<Person>(newHead.getValue());
-
-			dummyNewHead.setNext(guestInfo);
+			current.setNext(guestInfo);
+			current = current.getNext();
 
 			newHead = newHead.getNext();
 			guestCount++;
