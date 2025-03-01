405cdada5ffbe0bfabee07b59f64e5e5943ebfd4
Daniel Reiman <100288199+danielreiman@users.noreply.github.com>
2025-03-01 19:29:17 +0200

    Update Main.java

--- 2025-03-01 - HW/src/Main.java
+++ 2025-03-01 - HW/src/Main.java
@@ -2,46 +2,38 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Queue<Job> queue = new Queue<Job>();
-        
-        queue.insert(new Job(120, "WorkA"));
-        queue.insert(new Job(150, "WorkB"));
-        queue.insert(new Job(200, "WorkC"));
-        queue.insert(new Job(90, "WorkD"));
 
-        enoughTime(queue, 30 * 60);
 	}
-
-	public static void enoughTime(Queue<Job> jobs, int totalTime) {
-		int currentTime = 0;
-		jobs.insert(null);
-		while (jobs.head() != null) {
-			Job currentJob = jobs.remove();
-			
-			if (currentTime + currentJob.getDuration() < totalTime) {
-				currentTime += currentJob.getDuration();
-				
-				System.out.println(currentJob.codeWork);
+	
+	public static boolean ex1(int sum, Queue<Integer> q) {
+		q.insert(null);
+		boolean result = false;
+		while (q.head() != null) {
+			int x = q.remove();
+			if (hasPairSum(x, sum, q)) {
+				result = true;
 			}
-			
-			jobs.insert(currentJob);
+			q.insert(x + 1);
 		}
-		jobs.remove();
+		return result;
 	}
-	
-	// EX 9
-//	public static int delivery(int size, Queue<FoodDeliveryPerson> d) {
-//		d.insert(null);
-//		while (d.head() != null) {
-//			FoodDeliveryPerson current = d.remove();
-//			
-//			if (current.getSize() >= size) {
-//				return current.getID();
-//			}
-//			
-//			d.insert(current);
-//		}
-//		d.remove();
-//	}
-	
+
+	public static boolean hasPairSum(int num, int sum, Queue<Integer> q) {
+		q.insert(null);
+		boolean result = false;
+
+		while (q.head() != null) {
+			int x = q.remove();
+			if (x + num == sum) {
+				result = true;
+			}
+			q.insert(x);
+		}
+		
+		q.remove();
+
+		return result;
+	}
+
+
 }
