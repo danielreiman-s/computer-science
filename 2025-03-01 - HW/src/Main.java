91979bd40fde6d983ed758e39e2e87bdf42ae702
Daniel Reiman <100288199+danielreiman@users.noreply.github.com>
2025-03-02 10:53:19 +0200

    Update Main.java

--- 2025-03-01 - HW/src/Main.java
+++ 2025-03-01 - HW/src/Main.java
@@ -1,4 +1,4 @@
-
+// Version 01
 public class Main {
 
 	public static void main(String[] args) {
@@ -37,3 +37,64 @@ public class Main {
 
 
 }
+
+// Version 2
+public class Main {
+
+  public static void main(String[] args) {
+    Queue<Integer> q = new Queue<Integer>();
+    q.insert(1);
+    q.insert(2);
+    q.insert(3);
+    q.insert(4);
+
+    System.out.println(q);
+
+    System.out.println(ex1(10, q));
+
+    System.out.println(q);
+  }
+
+  public static boolean ex1(int sum, Queue<Integer> q) {
+    boolean result = false;
+    Queue<Integer> tempQueue = new Queue<>();
+    
+    while (!q.isEmpty()) {
+      int x = q.remove();
+      
+      if (hasPairSum(x, sum, q)) {
+        result = true;
+      }
+      
+      tempQueue.insert(x);
+    }
+    
+    while (!tempQueue.isEmpty()) {
+      q.insert(tempQueue.remove());
+    }
+    
+    return result;
+  }
+
+  public static boolean hasPairSum(int num, int sum, Queue<Integer> q) {
+    Queue<Integer> tempQueue = new Queue<>();
+    boolean result = false;
+    
+    while (!q.isEmpty()) {
+      int x = q.remove();
+      
+      if (x + num == sum) {
+        result = true;
+      }
+      
+      tempQueue.insert(x);
+    }
+    
+    while (!tempQueue.isEmpty()) {
+      q.insert(tempQueue.remove());
+    }
+    
+    return result;
+  }
+}
+
